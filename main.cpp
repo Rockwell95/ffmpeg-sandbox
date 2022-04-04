ac3b1ff65738c96516fd4822ddf7e8d303346a81
Dominick Mancini <dominick.mancini@kongsberggeospatial.com>
2022-04-04 12:57:37 -0400

    More messing with libav

--- main.cpp
+++ main.cpp
@@ -1,49 +1,65 @@
-#include <iostream>
-#include <fstream>
-#include <array>
 #include "Config.h"
+#include <array>
+#include <cstring>
+#include <execinfo.h>
+#include <fstream>
+#include <iostream>
+#include <signal.h>
 
 extern "C" {
 #include <libavcodec/avcodec.h>
-#include <libavformat/avformat.h>
 #include <libavdevice/avdevice.h>
+#include <libavformat/avformat.h>
 }
 
 using std::cout;
 using std::endl;
 using std::ifstream;
 
-int main(int argc, char *argv[]) {
-    AVFormatContext *pFormatCtx;
+void handler(int sig) {
+  void *array[10];
+  size_t size;
 
-    cout << "FFMPEG Sandbox Version " << FFMPEGSandbox_VERSION_MAJOR << "." << FFMPEGSandbox_VERSION_MINOR << endl;
-    cout << "Hello World!" << endl;
+  // get void*'s for all entries on the stack
+  size = backtrace(array, 10);
+
+  // print out all the frames to stderr
+  fprintf(stderr, "Error: signal %d:\n", sig);
+  backtrace_symbols_fd(array, size, STDERR_FILENO);
+  exit(1);
+}
+
+int main(int argc, char *argv[]) {
+  AVFormatContext *pFormatCtx = nullptr;
+
+  signal(SIGBUS, handler);
+
+  cout << "FFMPEG Sandbox Version " << FFMPEGSandbox_VERSION_MAJOR << "." << FFMPEGSandbox_VERSION_MINOR << endl;
 
 #if CONFIG_AVDEVICE
-    avdevice_register_all();
+  avdevice_register_all();
 #endif
-    avformat_network_init();
+  avformat_network_init();
+  cout << argv[1] << endl;
 
-    ifstream inFile;
-    inFile.open("/home/dmancini/Streams/t11.ts");
+  avformat_open_input(&pFormatCtx, argv[1], nullptr, nullptr);
 
-    avformat_open_input(&pFormatCtx, "/home/dmancini/Streams/t11.ts", nullptr, nullptr);
+  const AVStream *streams = *(pFormatCtx->streams);
+  const AVProgram *programs = *(pFormatCtx->programs);
+  char **buffer = new char *[2]();
+  for (int i = 0; i < 2; i++) {
+    buffer[i] = new char[256]();
+  }
+  av_dict_get_string(programs->metadata, buffer, '=', ';');
 
-    const AVStream *streams = *(pFormatCtx->streams);
-    char **buffer = new char *[2]();
-    for (int i = 0; i < 2; i++) {
-        buffer[i] = new char[256]();
-    }
-    av_dict_get_string(streams->metadata, buffer, '=', ';');
+  cout << *buffer << endl;
 
-    cout << *buffer << endl;
+  for (int i = 0; i < 2; i++) {
+    delete buffer[i];
+    buffer[i] = nullptr;
+  }
+  delete[] buffer;
+  buffer = nullptr;
 
-    for (int i = 0; i < 2; i++) {
-        delete buffer[i];
-        buffer[i] = nullptr;
-    }
-    delete[] buffer;
-    buffer = nullptr;
-
-    return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
